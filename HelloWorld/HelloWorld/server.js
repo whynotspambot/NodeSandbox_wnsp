aaea39a917e52aa01625e5a82054217ef9b13454
whynotspambot <mqueen.blue@gmail.com>
2017-04-06 11:20:59 -0700

    Lesson 1

--- HelloWorld/HelloWorld/server.js
+++ HelloWorld/HelloWorld/server.js
@@ -1,8 +1,1 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\nIt\'s a beautiful day!\n');
-}).listen(port);
+console.log('HELLO WORLD')

f7fd5d025f41d1ee33e5e73676ad9871e243570f
whynotspambot <mqueen.blue@gmail.com>
2017-04-07 13:41:37 -0700

    Lersson 9 with async module

--- HelloWorld/HelloWorld/server.js
+++ HelloWorld/HelloWorld/server.js
@@ -1,24 +1,35 @@
 // Lesson 9
 var http = require('http')
 var bl = require('bl')
+var async = require('async')
 
-var urls = process.argv.slice(2)
-var count = 0
-var results = []
-function processUrl(url, index) {
+function processUrl(url, callback) {
     http.get(url, function (response) {
         response.pipe(bl(function (err, data) {
             if (err) return console.error(err)
-            results[index] = data.toString()
-            count++
-            if (count == 3) {
-                for (var i = 0; i < 3; i++)
-                    console.log(results[i])
-            }
+            callback(null, data.toString())
         }))
     }).on("error", console.error)
 }
 
-for (var i = 0; i < urls.length; i++) {
-    processUrl(urls[i], i)
-}
+async.map(process.argv.slice(2), processUrl, function (err, results) {
+    for (var i = 0; i < results.length; i++)
+        console.log(results[i])
+})
+
+//for (var i = 0; i < urls.length; i++) {
+//    processUrl(urls[i], i)
+//}
+//var fs = require('fs')
+//var async = require('async')
+//function readF(filename, callback) {
+//    callback(null, 'hello')
+//}
+
+//async.map(['server.js', 'directoryFilter.js', 'package.json'], readF, function (err, results) {
+//    console.log('hello')
+//    for (var i = 0; i < results.length; i++) {
+//        console.log('hi')
+//        console.log(results[i])
+//    }
+//})

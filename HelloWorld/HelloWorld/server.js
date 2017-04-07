e6c93d204b2b2a7f9b443251dda4fd5fcf83d7ac
whynotspambot <mqueen.blue@gmail.com>
2017-04-07 12:04:42 -0700

    Lesson 7

--- HelloWorld/HelloWorld/server.js
+++ HelloWorld/HelloWorld/server.js
@@ -1,16 +1,9 @@
 // Lesson 7
-var df = require('./directoryFilter')
+var http = require('http')
 
-if (process.argv.length != 4){
-    console.log("Error.")
-    process.exit(1)
-}
-
-var directory = process.argv[2]
-var fileExtensionFilter = '.' + process.argv[3]
-df(process.argv[2], process.argv[3], function (err, files) {
-    if (err) {
-        return console.log(err)
-    }
-    files.forEach(function (file) { console.log(file) })
-})
+var url = process.argv[2]
+http.get(url, function (response) {
+    response.setEncoding('utf8')
+    response.on("data", console.log)
+    response.on("error", console.error)
+}).on("error", console.error)

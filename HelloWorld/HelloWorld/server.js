d0965d975fb6355f0c7aa04dd2ba69df7531687f
whynotspambot <mqueen.blue@gmail.com>
2017-04-07 14:03:44 -0700

    Lesson 10

--- HelloWorld/HelloWorld/server.js
+++ HelloWorld/HelloWorld/server.js
@@ -1,35 +1,17 @@
-// Lesson 9
-var http = require('http')
-var bl = require('bl')
-var async = require('async')
+// Lesson 10
+var net = require('net')
+var strftime = require('strftime')
 
-function processUrl(url, callback) {
-    http.get(url, function (response) {
-        response.pipe(bl(function (err, data) {
-            if (err) return console.error(err)
-            callback(null, data.toString())
-        }))
-    }).on("error", console.error)
+var portNumber = process.argv[2]
+
+function listener(socket) {
+    // new connection received
+    // send the time
+    var timeString = strftime("%Y-%m-%d %H:%M\n", new Date())
+    socket.end(timeString)
 }
 
-async.map(process.argv.slice(2), processUrl, function (err, results) {
-    for (var i = 0; i < results.length; i++)
-        console.log(results[i])
-})
+var server = net.createServer(listener)
+server.listen(portNumber)
 
-//for (var i = 0; i < urls.length; i++) {
-//    processUrl(urls[i], i)
-//}
-//var fs = require('fs')
-//var async = require('async')
-//function readF(filename, callback) {
-//    callback(null, 'hello')
-//}
-
-//async.map(['server.js', 'directoryFilter.js', 'package.json'], readF, function (err, results) {
-//    console.log('hello')
-//    for (var i = 0; i < results.length; i++) {
-//        console.log('hi')
-//        console.log(results[i])
-//    }
-//})
+//console.log(strftime("%Y-%m-%d %H:%M", new Date()))

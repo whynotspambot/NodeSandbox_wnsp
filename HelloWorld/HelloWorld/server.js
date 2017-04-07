5e0b2b0de9f3839a72e568c37dde6d59ba414905
whynotspambot <mqueen.blue@gmail.com>
2017-04-07 12:17:47 -0700

    Lesson 8

--- HelloWorld/HelloWorld/server.js
+++ HelloWorld/HelloWorld/server.js
@@ -1,9 +1,15 @@
-// Lesson 7
+// Lesson 8
 var http = require('http')
+var bl = require('bl')
 
 var url = process.argv[2]
+var charCount = 0
+var completeData = ''
 http.get(url, function (response) {
-    response.setEncoding('utf8')
-    response.on("data", console.log)
-    response.on("error", console.error)
+    response.pipe(bl(function (err, data) {
+        if (err) return console.error(err)
+        var completeData = data.toString()
+        console.log(completeData.length)
+        console.log(completeData)
+    }))
 }).on("error", console.error)
